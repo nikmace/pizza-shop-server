e9fe9856b3a4f3d8cd34c2a26b447e935999e49c
Nikita Baranov <nikmacentosh@gmail.com>
2022-06-06 18:33:20 +0300

    DTOs for pizza

---
+++ src/dto/pizza.dto.ts
@@ -0,0 +1,10 @@
+export interface IPizzaDto {
+  id: string;
+  imageUrl: string;
+  name: string;
+  types: number[];
+  sizes: number[];
+  price: number;
+  category: number;
+  rating: number;
+}
