bcb226058c9fa4979f61c033ecf16641cb00ce46
Nikita Baranov <nikmacentosh@gmail.com>
2022-06-12 20:38:41 +0300

    Refurbished order controller and service

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,13 +2,25 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 import { OrderService } from './order.service';
 
-export type OrderInputValues = {
+export type OrderCartData = {
+  name: string;
+  variationId: string;
+  count: number;
+  price: number;
+  size: number;
+  type: string;
+};
+
+export type Order = {
   firstName: string;
   lastName: string;
   phone: string;
   email: string;
   address: string;
   time: string;
+  order: OrderCartData[];
+  orderTimeSent: string;
+  totalPrice: number;
 };
 
 @Controller('/order')
@@ -16,7 +28,7 @@ export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
-  getAll(@Body() body: OrderInputValues) {
+  getAll(@Body() body: Order) {
     return this.orderService.validateInput(body);
   }
 }
